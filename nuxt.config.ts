33f795a44911d1f1dd670c767836f61832a1e2a3
Harm Matthias Harms <harmmatthias.harms@gmail.com>
2024-08-25 08:09:25 +0200

    Add datapipeline section (#232)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    devtools: {enabled: true},
-    modules: ['@nuxt/ui', '@nuxtjs/color-mode', "@nuxt/image"],
-    ui: {
-        icons: ['heroicons', 'simple-icons']
-    },
-    nitro: {
-        firebase: {
-            gen: 2
-        }
-    }
+  devtools: {enabled: true},
+  modules: ['@nuxt/ui', '@nuxtjs/color-mode', "@nuxt/image"],
+
+  ui: {
+      icons: ['heroicons', 'simple-icons']
+  },
+
+  nitro: {
+      firebase: {
+          gen: 2
+      }
+  },
+
+  compatibilityDate: '2024-08-25'
 })
