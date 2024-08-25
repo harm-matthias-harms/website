6d7f833528d21c8a01d3f45be30a5b56959cf809
Harm Matthias Harms <harmmatthias.harms@gmail.com>
2024-08-25 08:22:40 +0200

    Revert "Add datapipeline section (#232)"
    This reverts commit 33f795a44911d1f1dd670c767836f61832a1e2a3.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: {enabled: true},
-  modules: ['@nuxt/ui', '@nuxtjs/color-mode', "@nuxt/image"],
-
-  ui: {
-      icons: ['heroicons', 'simple-icons']
-  },
-
-  nitro: {
-      firebase: {
-          gen: 2
-      }
-  },
-
-  compatibilityDate: '2024-08-25'
+    devtools: {enabled: true},
+    modules: ['@nuxt/ui', '@nuxtjs/color-mode', "@nuxt/image"],
+    ui: {
+        icons: ['heroicons', 'simple-icons']
+    },
+    nitro: {
+        firebase: {
+            gen: 2
+        }
+    }
 })
