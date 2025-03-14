379ee6734e053575dc38e1633466e5e7cd475f0f
Harm Matthias Harms <matthias.harms@quis.de>
2025-03-14 15:54:58 +0100

    Follow migration guide

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,18 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
   modules: ['@nuxt/ui', '@nuxtjs/color-mode', "@nuxt/image"],
-
+  css: ['~/assets/css/main.css'],
   ui: {
     icons: ['heroicons', 'simple-icons']
   },
-
   nitro: {
     firebase: {
       gen: 2
     }
   },
-
   compatibilityDate: '2024-08-25'
 })
