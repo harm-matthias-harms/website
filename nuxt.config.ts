b384dac72c495aa26f9f13f46d102930c1d291ba
Harm Matthias Harms <harmmatthias.harms@gmail.com>
2025-05-28 22:08:21 +0200

    Remove firebase deployment (#407)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  ssr: false,
   modules: ['@nuxt/ui', '@nuxtjs/color-mode', "@nuxt/image"],
   css: ['~/assets/css/main.css'],
-  ui: {
-    icons: ['heroicons', 'simple-icons']
-  },
   nitro: {
-    firebase: {
-      gen: 2
+    prerender: {
+      routes: [
+        '/_ipx/_/matthiasharms-dark.jpg',
+        '/_ipx/_/matthiasharms.jpg',
+      ]
     }
   },
   compatibilityDate: '2024-08-25'
